9086af946cfe99f6dab6b1a158b963273f2ec1d3
hiro <kckyy292@gmail.com>
2020-07-09 23:41:10 +0900

    modal追加

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,15 @@
-console.log("test");
+let openModal = function(){
+  $("._modal_open").on("click", function(){
+    // modal open
+    $("._modal").fadeIn();
+  });
+
+  $("._modal_close").on("click", function(){
+    // close modal
+    $("._modal").fadeOut();
+  });
+}
+
+$(function(){
+  openModal();
+});
